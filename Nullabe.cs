7f4ce9c6800a4a6c41eb8dc34ad2dde2f1742327
Matheus Sobral <matheusalucard.js@gmail.com>
2024-06-20 09:52:26 -0300

    Update Nullabe.cs

--- Nullabe.cs
+++ Nullabe.cs
@@ -4,17 +4,17 @@ class Sim {
     static void Main(string[] args) {
     double? x = null;
     double? y = 10.0;
-Console.WriteLine(x.GetValueOrDefault());
-Console.WriteLine(y.GetValueOrDefault());
-Console.WriteLine(x.HasValue);
-Console.WriteLine(y.HasValue);
-if (x.HasValue)
-Console.WriteLine(x.Value);
-else
-Console.WriteLine("X is null");
-if (y.HasValue)
-Console.WriteLine(y.Value);
-else
-Console.WriteLine("Y is null");
-}
+    Console.WriteLine(x.GetValueOrDefault());
+    Console.WriteLine(y.GetValueOrDefault());
+    Console.WriteLine(x.HasValue);
+    Console.WriteLine(y.HasValue);
+    if (x.HasValue)
+    Console.WriteLine(x.Value);
+    else
+    Console.WriteLine("X is null");
+    if (y.HasValue)
+    Console.WriteLine(y.Value);
+    else
+    Console.WriteLine("Y is null");
+    }
 }*/
